d961f930243d2fb1928719a978defa632e0541ba
Emilio Funes R. <emifuri@outlook.com>
2026-03-20 21:48:10 -0600

    Carpeta y archivos vacios

---
+++ programa/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Esto es un mensaje de prueba.\n");
+    printf("Presiona ENTER para salir...");
+    getchar();
+    return 0;
+}
